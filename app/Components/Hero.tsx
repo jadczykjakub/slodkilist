7da2d102a71e00450bda7af2f6b72f9123def00a
jadczykajakub <jadczyk.jakub@gmail.com>
2024-03-03 18:06:08 +0100

    final

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
           <span className="text-primary font-weight-medium">Name</span>
         </h1>
         <p className="text-muted mb-4 pb-2">
-          Lorem Ipsum has been the industry's standard dummy text ever since the
+          Lorem Ipsum has been the industrys standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
           to make a type specimen book. It has survived not only five centuries,
           but also the leap into electronic typesetting, remaining essentially
